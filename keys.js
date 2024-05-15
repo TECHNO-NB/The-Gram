6043e32777ccee09a7a623e826d5b53e912dd297
NareshBhattarai4 <technologyandapplication@gmail.com>
2024-05-15 13:10:05 +0545

    new insta

---
+++ keys.js
@@ -0,0 +1,7 @@
+
+
+
+module.exports = {
+    mongoUrl:"mongodb+srv://instaclone:hsqLFU4cRzn8sMPh@cluster0.xdxfjaw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+    Jwt_secret :"instaclone%*&%*%^#@&#^gdidSIGAGDUOSHFKF"
+}
